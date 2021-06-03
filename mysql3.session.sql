f351f1eacc8d03746d1ebe3d9b21c36373133fa5
MNG-source <66915215+MNG-source@users.noreply.github.com>
2021-06-03 21:31:29 +0530

    Add files via upload

---
+++ mysql3.session.sql
@@ -0,0 +1,2 @@
+CREATE TABLE products (id INT, descr VARCHAR(255), price INT);
+INSERT into products (id,descr,price) VALUES (1,mhgfmjhf,54);
---
+++ root.session.sql
@@ -0,0 +1,2 @@
+INSERT INTO product_info (p_id, p_name)
+VALUES (1234, 'book');
